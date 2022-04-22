69434eab98a9f3badf521edc3216862a6f65aa2e
Theresia Tawk <theresiatawk@hotmail.com>
2022-04-22 16:12:28 +0300

    Reproducing Serial Code on an array size of 1000

--- count3s-pthread.c
+++ count3s-pthread.c
@@ -1,88 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <pthread.h>
 
-#define MaxThreads 1000
-void* count3s_thread(void* id);
-pthread_t tid[MaxThreads];
-
-int t;         /* number of threads */
-int * array;
+int* array;
 int length;
-int count;
 
-struct padded_int
+int main()
 {
-  int value;
-  char padding[60];
-} private_count[MaxThreads];
-pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
+    int i;
+    length = 1000;
+    int count = 0;
+    array = calloc(length, sizeof(int));
 
-void count3s()
-{
-   int i;
-   count = 0;
-   /* Create t threads */
-   for(i = 0; i < t; i++)
-   {
-      pthread_create(&tid[i], NULL, count3s_thread, (void*)i);
-   }
+    /* initialize the array with random integers between 0 and 9 */
+    srand(time(NULL));  /* seed the random number generator with current time */
 
-   /*** wait for the threads to finish ***/
-   for(i = 0; i < t; i++)
-   {
-      pthread_join(tid[i], NULL);
-   }
-}
+    /* start the time of execution */
+    clock_t time_start = clock();
+    for (i = 0; i < length; i++)
+    {
+        array[i] = rand() % 10;
+    }
+    for (i = 0; i < length; i++)
+    {
+        if (array[i] == 3)
+        {
+            count++;
+        }
+    }
+    clock_t time_end = clock();
 
-void* count3s_thread(void* id)
-{
-   int i;
-   /* Compute portion of the array that this thread should work on */
-   int length_per_thread = length / t;
-   int start = (int)id * length_per_thread;
+    /*Caculating teh time it takes for execution*/
 
-   for(i = start; i < start+length_per_thread; i++)
-   {
-      if(array[i] == 3)
-      {
-         private_count[(int)id].value++;
-      }
-   }
-   pthread_mutex_lock(&m);
-   count += private_count[(int)id].value;
-   pthread_mutex_unlock(&m);
-
-   return 0;
-}
-
-
-int main(int argc, char *argv[])
-{
-   int i;
-   length = 1048576;  /*  2^20  */
-   t = 64;  /*** be sure that t divides length!! ***/
-
-   array = calloc(length, sizeof(int));
-
-   /* initialize the array with random integers between 0 and 9 */
-   srand(time(NULL));  /* seed the random number generator with current time */
-   for(i = 0; i < length; i++)
-   {
-      array[i] = rand()%10;
-   }
-
-   count3s();  /* run the threads, wait till they finish */
-
-   printf("The number of 3's is %d\n", count);
-
-   /* As a test, let us count 3's the slow, serial way. */
-   count = 0;
-   for(i = 0; i < length; i++)
-      if(array[i] == 3)
-         count++;
-   printf("The number of 3's is %d\n", count);
-
-   return 0;
+    double time_spent = ((double)(time_end - time_start) / CLOCKS_PER_SEC) * 1000.0;
+    printf("There are %d 3s in the array and the running time is:  %fms", count, time_spent);
+    return 0;
 }
